bc06ddcb46b20204f6c331f48108e8ce396bbe9c
Martijn <martijnvermeulen1@gmail.com>
2022-03-10 21:29:10 +0100

    moved hamburger menu to lower navbar, made fontsizes shrink for upper-bar links

--- wp-content/themes/blank-canvas/template-parts/header/header-content.php
+++ wp-content/themes/blank-canvas/template-parts/header/header-content.php
@@ -11,16 +11,12 @@ if (sizeof($menus) > 0) {
 ?>
 <header class="header">
   <div class="navbar upper-navbar">
-    <object class="logo" data="/resources/heartscan2.svg" width="60" height="40"> </object>
-    <input id="menu-toggle" type="checkbox"/>
+    <object class="logo" data="/resources/sidekiq.svg" width="60" height="40"> </object>
     <ul class="nav-links upper-navbar-links">
       <li><a href="#">Life Being Inspiration</a></li>
       <li><a href="#">Being Child Illustrations</a></li>
       <li><a href="#">Life Being Atelier</a></li>
     </ul>
-    <label for="menu-toggle" class="hamburger-container">
-      <span class="hamburger-menu"></span>
-    </label>
   </div>
   <div class="title-with-btns">
     <i class="fas fa-chevron-left prev-button"></i>
@@ -31,11 +27,15 @@ if (sizeof($menus) > 0) {
     <i class="fas fa-chevron-right next-button"></i>
   </div>
   <div class="navbar lower-navbar">
+    <input id="menu-toggle" type="checkbox"/>
     <ul class="nav-links lower-navbar-links">
         <?php foreach ($menu_items as $item){
         echo "<li><a href=" . $item -> url . ">" . $item -> title . "</a></li>";
             }
         ?>
     </ul>
+    <label for="menu-toggle" class="hamburger-container">
+      <span class="hamburger-menu"></span>
+    </label>
   </div>
 </header>
