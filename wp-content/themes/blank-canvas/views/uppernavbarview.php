481cd5d0d64e6f58bbd6ae0bd7893a75804048e9
Jessica <jazzyVos@yahoo.com>
2022-08-21 20:12:56 +0200

    removed dead links

--- wp-content/themes/blank-canvas/views/uppernavbarview.php
+++ wp-content/themes/blank-canvas/views/uppernavbarview.php
@@ -7,8 +7,6 @@ class UpperNavBarView implements IView {
     <ul class="nav-links upper-navbar-links">
       <li><a href="/">Be Home</a></li>
       <li><a href="/being-me-being-you-2">Life Being Inspirations</a></li>
-      <li><a href="#">Being Child Illustrations</a></li>
-      <li><a href="#">Life Being Atelier</a></li>
     </ul>
     EOL;
   }
