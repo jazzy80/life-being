cb8569236f0f9fde3c3e60c573416b553a0f35fa
Jessica <jazzyVos@yahoo.com>
2022-11-06 21:29:18 +0100

    added images for the gallery main page

--- wp-content/themes/blank-canvas/views/atelierhomeview.php
+++ wp-content/themes/blank-canvas/views/atelierhomeview.php
@@ -9,23 +9,23 @@ class AtelierHomeView implements IView {
     return <<< EOL
     <main class="main">
         <div class="image-container">
-            <img class="image" src="/resources/boom_in_mist.jpg">
+            <img class="image" src="/resources/nature-art.jpg">
             <a href="/life-being-gallery?category=nature-art" class="image-caption"> Nature Art Photography</a>
         </div>
         <div class="image-container">
             <a href="/life-being-gallery?category=flowers-of-beauty" class="image-caption">Flowers of beauty</a>
-            <img class="image" src="/resources/boom_in_mist.jpg">
+            <img class="image" src="/resources/flowers-of-beauty.jpg">
         </div>
         <div class="image-container">
-            <img class="image" src="/resources/boom_in_mist.jpg">
+            <img class="image" src="/resources/poetry-art-kleurrijk.jpg">
             <a href="/life-being-gallery?category=poetry-art" class="image-caption"> Poetry Art</a>
         </div>
         <div class="image-container">
             <a href="/life-being-gallery?category=being-art" class="image-caption">Being Arts</a>
-            <img class="image" src="/resources/boom_in_mist.jpg">
+            <img class="image" src="/resources/being-art-communicatie-bomen.png">
         </div>
         <div class="image-container">
-            <img class="image" src="/resources/boom_in_mist.jpg">
+            <img class="image" src="/resources/light-paintings.jpg">
             <a href="/life-being-gallery?category=light-paintings" class="image-caption">Light Paintings</a>
         </div>
     </main>
