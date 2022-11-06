841b18ca7fe4eeb4943b9b9a188dfd196e5dc6db
Jessica <jazzyVos@yahoo.com>
2022-11-06 13:20:41 +0100

    small css refactor to atelier

--- wp-content/themes/blank-canvas/views/atelierhomeview.php
+++ wp-content/themes/blank-canvas/views/atelierhomeview.php
@@ -13,20 +13,20 @@ class AtelierHomeView implements IView {
             <a href="/life-being-gallery?category=nature-art" class="image-caption"> Nature Art Photography</a>
         </div>
         <div class="image-container">
+            <a href="/life-being-gallery?category=flowers-of-beauty" class="image-caption">Flowers of beauty</a>
+            <img class="image" src="/resources/boom_in_mist.jpg">
+        </div>
+        <div class="image-container">
+            <img class="image" src="/resources/boom_in_mist.jpg">
             <a href="/life-being-gallery?category=poetry-art" class="image-caption"> Poetry Art</a>
-            <img class="image" src="/resources/boom_in_mist.jpg">
         </div>
         <div class="image-container">
-            <img class="image" src="/resources/boom_in_mist.jpg">
             <a href="/life-being-gallery?category=being-art" class="image-caption">Being Arts</a>
+            <img class="image" src="/resources/boom_in_mist.jpg">
         </div>
         <div class="image-container">
+            <img class="image" src="/resources/boom_in_mist.jpg">
             <a href="/life-being-gallery?category=light-paintings" class="image-caption">Light Paintings</a>
-            <img class="image" src="/resources/boom_in_mist.jpg">
-        </div>
-        <div class="image-container">
-            <img class="image" src="/resources/boom_in_mist.jpg">
-            <a href="/life-being-gallery?category=art-products" class="image-caption">Art Products</a>
         </div>
     </main>
     EOL;
