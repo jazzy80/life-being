1c123a11c5533d61d699d4b4f7a1cd332d82166b
Jessica <jazzyVos@yahoo.com>
2023-01-15 21:15:11 +0100

    removed spinning loader

--- scripts/dist/bigdreamsmain.js
+++ scripts/dist/bigdreamsmain.js
@@ -12,72 +12,68 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 function init() {
     return __awaiter(this, void 0, void 0, function* () {
         let currentPage = 0;
-        const prevButtons = document.querySelectorAll(".prev-btn");
-        const nextButtons = document.querySelectorAll(".next-btn");
-        setUpPage(currentPage, [prevButtons, nextButtons]);
-        prevButtons.forEach((prevButton) => {
-            prevButton.addEventListener("click", () => {
-                setUpPage(--currentPage, [prevButtons, nextButtons]);
-            });
+        const category = generateCategoryParam();
+        const prevButton = document.querySelector(".prev-btn");
+        const nextButton = document.querySelector(".next-btn");
+        setTitle(category.split("=")[1]);
+        setUpPage(category, currentPage, [prevButton, nextButton]);
+        prevButton.addEventListener("click", () => {
+            setUpPage(category, --currentPage, [prevButton, nextButton]);
         });
-        nextButtons.forEach((nextButton) => {
-            nextButton.addEventListener("click", () => {
-                setUpPage(++currentPage, [prevButtons, nextButtons]);
-            });
+        nextButton.addEventListener("click", () => {
+            setUpPage(category, ++currentPage, [prevButton, nextButton]);
         });
     });
 }
-function setUpPage(currentPage, buttons) {
+function setUpPage(category, currentPage, buttons) {
     return __awaiter(this, void 0, void 0, function* () {
-        const body = yield fetchImages(currentPage);
+        const body = yield fetchImages(category, currentPage);
         const imageSrcs = body.images;
-        const amountOfPages = imageSrcs.length === 0 ? 0 : Math.ceil(body.count / 9);
+        const paginationSize = body.paginationSize;
+        const amountOfPages = imageSrcs.length === 0 ? 0 : Math.ceil(body.count / paginationSize);
         const imageContainer = document.querySelector(".bigdreams-images");
         imageContainer.innerHTML = "";
-        const loader = document.querySelector(".loader");
-        const content = document.querySelector(".content");
-        loader.style.display = "block";
-        content.style.display = "none";
-        setPaginationText(currentPage, imageSrcs.length, body.count);
+        setPaginationText(currentPage, imageSrcs.length, body.count, paginationSize);
         setButtons(buttons, currentPage, amountOfPages);
-        const images = yield Promise.all(imageSrcs.map(createImageFromSrc));
-        images.forEach((image) => imageContainer.appendChild(image));
-        loader.style.display = "none";
-        content.style.display = "flex";
+        imageSrcs
+            .map(createImageFromSrc)
+            .forEach((image) => imageContainer.appendChild(image));
     });
 }
-function fetchImages(page) {
+function fetchImages(category, page) {
     return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch(`/wp-json/api/atelier/?category=big-dreams&page=${page}`);
+        const response = yield fetch(`/wp-json/api/atelier/${category}&page=${page}`);
         return response.json();
     });
 }
-function setPaginationText(currentPage, items, count) {
+function setPaginationText(currentPage, items, count, paginationSize) {
     const paginationText = document.querySelector(".pagination-text");
-    const min = currentPage * 9 + 1;
+    const min = currentPage * paginationSize + 1;
     const max = min + items - 1;
     paginationText.textContent = `Foto's ${min} t/m ${max} getoond van de ${count}`;
 }
 function setButtons(buttons, currentPage, amountOfPages) {
-    const [prevs, nexts] = buttons;
-    prevs.forEach((prev) => {
-        enableButton(prev);
-        if (currentPage === 0)
-            disableButton(prev);
-    });
-    nexts.forEach((next) => {
-        enableButton(next);
-        if (currentPage === amountOfPages - 1)
-            disableButton(next);
-    });
+    const [prev, next] = buttons;
+    enableButton(prev);
+    enableButton(next);
+    if (currentPage === 0)
+        disableButton(prev);
+    if (currentPage === amountOfPages - 1)
+        disableButton(next);
+    buttons.forEach((b) => (b.style.visibility = "visible"));
 }
 function disableButton(button) {
     button.classList.add("paginator-btn-disabled");
-    button.style.visibility = "hidden";
+    button.disabled = true;
 }
 function enableButton(button) {
     button.classList.remove("paginator-btn-disabled");
-    button.style.visibility = "visible";
+    button.disabled = false;
+}
+function setTitle(category) {
+    const title = category.replace(/-/g, " ");
+    const titleElement = document.querySelector(".bigdreams-title");
+    titleElement.textContent = title;
 }
 function createImageFromSrc(src) {
     const imageFrame = document.createElement("div");
@@ -85,12 +81,12 @@ function createImageFromSrc(src) {
     const imageText = document.createElement("span");
     const image = new Image();
     imageFrame.append(image);
-    return new Promise((resolve) => {
-        image.src = src;
-        image.onload = () => {
-            resolve(imageFrame);
-        };
-    });
+    image.src = src;
+    return imageFrame;
+}
+function generateCategoryParam() {
+    const categoryParam = new URLSearchParams(location.search).get("category");
+    return categoryParam ? `?category=${categoryParam}` : "";
 }
 init();
 
