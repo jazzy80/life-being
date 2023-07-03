8a5c0c4a6d7b5a8e50ecb5326fec13edd739ead1
Jessica <jazzyVos@yahoo.com>
2023-07-03 21:15:27 +0200

    endpoints implemented to fetch pages

--- wp-content/themes/blank-canvas/api/pages.php
+++ wp-content/themes/blank-canvas/api/pages.php
@@ -10,11 +10,20 @@ add_action('rest_api_init', function () {
 } );
 
 add_action('rest_api_init', function (){
-
+  register_rest_route( 'api/', 'pages/(?<id>\d+)/', array(
+    'methods' => 'GET',
+    'callback' => 'get_page_by_id'
+  ));
 } );
 
 function get_all_pages(WP_REST_Request $req): array {
   global $wpdb;
   $page_model = new PageModel($wpdb);
   return $page_model -> get_all_pages();
+}
+
+function get_page_by_id(WP_REST_Request $req): object{
+  global $wpdb;
+  $page_model = new PageModel($wpdb);
+  return $page_model -> get_page_by_id($req['id']);
 }
--- wp-content/themes/blank-canvas/models/pagemodel.php
+++ wp-content/themes/blank-canvas/models/pagemodel.php
@@ -9,7 +9,25 @@ class PageModel {
 
   public function get_all_pages(): array {
     return $this -> db_client -> get_results(
-      "SELECT * FROM wp_posts WHERE post_type = 'page' AND post_status = 'publish';",
+      <<< EOL
+      SELECT ID, post_title, guid
+      FROM wp_posts 
+      WHERE post_type = 'page' AND post_status = 'publish';
+      EOL,
+      ARRAY_A
+    );
+  }
+
+  public function get_page_by_id(int $id): object{
+    return $this -> db_client -> get_row(
+      $this -> db_client -> prepare(
+        <<< EOL
+        SELECT ID, post_title, guid, post_content
+        FROM wp_posts
+        WHERE ID = %s
+        EOL,
+        array($id)
+      ),
     );
   }
 }
